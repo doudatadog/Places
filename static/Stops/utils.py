61ad23a0d994eb38bf66d43958eaa9dee3ecab35
Abdourahmane TINTOU <abdourahmane@gmail.com>
2019-01-21 17:45:53 +0100

    Update utils.py

--- static/Stops/utils.py
+++ static/Stops/utils.py
@@ -1,13 +1,12 @@
-from opencage.geocoder 				import OpenCageGeocode
-import 								sys,pandas,math
-from scipy.spatial      			import distance
+from opencage.geocoder             import OpenCageGeocode
+import                             sys,pandas,numpy as np
+from scipy.spatial                 import distance
 
-dlim								= 0.024960428141358584#euclidian dist for 31 mins
+dlim                                = 0.024960428141358584#euclidian dist for 31 mins
 
 def Geocode(address,apikey) 		: 
-	geocoder 						= OpenCageGeocode(apikey)
-	result 							= geocoder.geocode(address)
-	result							= (None,)*2 if len(result) == 0 else result[0][u'geometry'].values()
-	return 							  result
-def Timer(x) 					    :
-	return 							pandas.np.ceil(x*(31/dlim))
+	geocoder                        = OpenCageGeocode(apikey)
+	result                          = geocoder.geocode(address)
+	result                          = (0.,)*2 if len(result) == 0 else result[0][u'geometry'].values()
+	return                          result
+def Timer(x) 					    : return np.ceil(x*(31/dlim));
