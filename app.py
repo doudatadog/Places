ec4ad8735d5876e7679d4df551b1ec5431d56737
Abdourahmane TINTOU <abdourahmane@gmail.com>
2019-01-21 17:48:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from   flask 				import request,Flask, render_template
-from static.Stops   		import geocode_stops
+from   flask                import request,Flask, render_template
+from static.Stops           import geocode_stops
 
-app 						= Flask(__name__)
+app                         = Flask(__name__)
 
 
 @app.route('/')
-def home()					:
-	return 					render_template('home.html')
+def home()                  : return render_template('home.html')
 
 @app.route('/geocode',methods=['POST'])
-def geocode()				:
-	dep,arr         		= request.form.values()
-	result					= geocode_stops.Processe(dep,arr)
-	return 					render_template('result.html',result=result['arrival']['stops'])
+def geocode()               :
+	dep,arr                 = request.form.values()
+	result                  = geocode_stops.Processe(dep,arr)
+	return                    render_template('result.html',result=result['arrival']['stops'])
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__'  :
 	app.run(debug = True)
