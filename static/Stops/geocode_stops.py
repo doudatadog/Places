9d3b4289bf1f1a20346076716aa8059f42fa8646
Abdourahmane TINTOU <abdourahmane@gmail.com>
2019-01-21 17:38:01 +0100

    cleansed script

--- static/Stops/geocode_stops.py
+++ static/Stops/geocode_stops.py
@@ -1,24 +1,15 @@
 import utils,nearby_stops
 
-apikey                                  = '95180978743a4a4a843f2a47647bd4ae'
+apikey                          = '95180978743a4a4a843f2a47647bd4ae'
 
 
-def 		Processe(dep,arr)						:
+def 		Processe(dep,arr)	:
 			
-			d,a 									=dep,arr
-
-
-			infos 									= [('departure',d),('arrival',a)]
-
-			Geo 									= lambda X : (X[0],utils.Geocode(X[1],apikey) )
-
-			infos									= dict(map(Geo,infos))
-
-
-			departures,arrivals						=  nearby_stops.compute_nearest([infos['departure'],infos['arrival']])
-
-			infos['arrival']						=  {'latlon':infos['arrival'],
-														'stops':arrivals.fillna(0).apply(lambda x : [x['stop_name'],x['stop_lat'],x['stop_lon']],1).tolist()}
-			infos['departure']						=  {'geo':infos['departure'],
-														'stops':departures.fillna(0).apply(lambda x : [x['stop_lat'],x['stop_lon']],1).tolist()}
-			return 										infos	
+			d,a 				= dep,arr
+			infos 				= [('departure',d),('arrival',a)]
+			Geo 				= lambda X : (X[0],utils.Geocode(X[1],apikey) )
+			infos				= dict(map(Geo,infos))
+			departures,arrivals	= nearby_stops.compute_nearest([infos['departure'],infos['arrival']])
+			infos['arrival']	= {'latlon':infos['arrival'],'stops':arrivals.fillna(0).apply(lambda x : [x['stop_name'],x['stop_lat'],x['stop_lon']],1).tolist()}
+			infos['departure']	= {'geo':infos['departure'],'stops':departures.fillna(0).apply(lambda x : [x['stop_lat'],x['stop_lon']],1).tolist()}
+			return 				  infos	
--- static/Stops/nearby_stops.py
+++ static/Stops/nearby_stops.py
@@ -1,28 +1,14 @@
 import utils
 
 def 	compute_nearest(latlons)										:
-
 		stops  															= utils.pandas.read_csv('./static/Stops/data/stops.txt')
-
 		coords 															= stops[['stop_lat','stop_lon']].values
-
 		result 															= utils.distance.cdist(latlons,coords)
-
 		result 															= utils.pandas.DataFrame(result).T
-
 		result.columns 													= ['departure','arrival']
-
-		result 															= utils.pandas.concat([stops,result],axis=1).assign(
-
-
-			va 															= lambda DF : True,
-			vd 															= lambda DF : True)
-
-		arrivals   														= result[result.va].sort_values('arrival').head(10)
-
-		departures 														= result[result.vd].sort_values('departure').head(10)
-
+		result 															= utils.pandas.concat([stops,result],axis=1)
+		arrivals   														= result.sort_values('arrival').head(10)
+		departures 														= result.sort_values('departure').head(10)
 		arrivals['minutes'] 											= arrivals.arrival.apply(utils.Timer)
 		departures['minutes'] 											= departures.departure.apply(utils.Timer)
-
 		return  departures,arrivals
